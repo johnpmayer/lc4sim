44f402e2ea0b70c2234e1e0797db66d7d93c6475
John P Mayer, Jr <john.p.mayer.jr@gmail.com>
2011-12-05 14:13:59 -0500

    moved imm

--- lc4vm.hs
+++ lc4vm.hs
@@ -7,6 +7,8 @@ import Data.Int (Int16)
 import Data.Map (Map)
 import Data.Set (Set)
 
+import Immediate
+
 type LineNumber = Int
 type Label = String
 
@@ -44,41 +46,6 @@ data CC = CC_N | CC_Z | CC_P deriving Eq
 type PC = (Imm U16)
 
 type VMState = (Program, Labels, RegisterFile, Memory, Breakpoints, PC, CC)
-
-data I5
-data I6
-data I7
-data I9
-data I16
-data U4
-data U7
-data U8
-data U16
-
-data Imm n where
-  IMM5   :: Int -> Imm I5
-  IMM6   :: Int -> Imm I6
-  IMM7   :: Int -> Imm I7
-  IMM9   :: Int -> Imm I9
-  IMM16  :: Int -> Imm I16
-  UIMM4  :: Int -> Imm U4
-  UIMM7  :: Int -> Imm U7
-  UIMM8  :: Int -> Imm U8
-  UIMM16 :: Int -> Imm U16
-
-instance Eq (Imm n) where
-  (IMM5 n1) == (IMM5 n2) = n1 == n2
-  (IMM6 n1) == (IMM6 n2) = n1 == n2
-  (IMM7 n1) == (IMM7 n2) = n1 == n2
-  (IMM9 n1) == (IMM9 n2) = n1 == n2
-  (IMM16 n1) == (IMM16 n2) = n1 == n2
-  (UIMM4 n1) == (UIMM4 n2) = n1 == n2
-  (UIMM7 n1) == (UIMM7 n2) = n1 == n2
-  (UIMM8 n1) == (UIMM8 n2) = n1 == n2
-  (UIMM16 n1) == (UIMM16 n2) = n1 == n2
-
-instance Ord (Imm n) where
-  (IMM16 n1) `compare` (IMM16 n2) = n1 `compare` n2
 
 data BC = N
         | NZ
