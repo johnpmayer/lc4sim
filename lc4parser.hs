15884ad4538d11b3dfa0b242d64f22b29e92c3d3
Mishal Awadah <a.mamish@gmail.com>
2011-12-05 16:11:05 -0500

    parser instruction complete

--- lc4parser.hs
+++ lc4parser.hs
@@ -5,12 +5,26 @@ import LC4VM
 import Parser
 import ParserCombinators
 
--- Parse the lines left to write and delegate to another parser
--- as necessary
 
 -- | Parse a String as a specific datatype. 
 constP :: String -> a -> Parser a
 constP s a = string s >> return a
+
+-- | Parses an LC4 hexadecimal representation.
+hexP :: Parser Int
+hexP = do _ <- char 'x'
+          i <- int
+          return i
+
+-- | Parses an LC4 decimal representation. 
+decP :: Parser Int
+decP = do _ <- char '#'
+          i <- int
+          return i
+          
+-- | Parses an LC4 integer of any representation.
+intP :: Parser Int
+intP = hexP <|> decP
 
 -- | Parse a line as either insn, comment, or directive.
 lineP :: Parser Line
@@ -25,20 +39,35 @@ triOpP = constP "ADD" ADD <|>
          constP "SUB" SUB <|> 
          constP "DIV" DIV <|>
          constP "AND" AND <|> 
-         constP "OR" OR <|>
+         constP "OR"  OR  <|>
          constP "XOR" XOR <|>
          constP "MOD" MOD
 
 -- | Parses an LC4 operation that expects 2 register arguments
 biOpP :: Parser (Register -> Register -> Instruction)
-biOpP = constP "CMP" CMP <|>
+biOpP = constP "CMP"  CMP  <|>
         constP "CMPU" CMPU <|>
-        constP "NOT" NOT
+        constP "NOT"  NOT
+
+-- | Parses an LC4 operation that expects 1 register argument
+uOpP :: Parser (Register -> Instruction) 
+uOpP = constP "JSRR" JSRR <|>
+       constP "JMPR" JMPR
+       
+-- | Parses an LC4 operation that expects 2 register and 1 IMM arguments.
+triOpImmP :: Parser (Register -> Register -> Int -> Instruction)
+triOpImmP = constP "ADD" ADDI <|>
+            constP "AND" ANDI <|>
+            constP "LDR" LDR  <|>
+            constP "STR" STR  <|>
+            constP "SLL" SLL  <|>
+            constP "SRA" SRA  <|>
+            constP "SRL" SRL
 
 -- | Parses an LC4 Instruction. 
 insnP :: Parser Instruction
 insnP = choice [constInsnP, brP, triRegOpP, duoRegOpP, unoRegOpP,
-                duoRegImmOpP, regImmOpP]
+                duoRegImmOpP, regImmOpP, regLblOpP, lblOpP]
         where
           -- Instructions that take no args
           constInsnP = choice [constP "NOP" NOP,
@@ -60,11 +89,34 @@ insnP = choice [constInsnP, brP, triRegOpP, duoRegOpP, unoRegOpP,
                r2 <- regP
                return $ op r1 r2
           -- Insns that take 1 reg arg
-          unoRegOpP = undefined
+          unoRegOpP = 
+            do op <- uOpP
+               r1 <- regP
+               return $ op r1
           -- Insns that take 2 reg and 1 Imm arg
-          duoRegImmOpP = undefined
+          duoRegImmOpP = 
+            do op <- triOpImmP
+               r1 <- regP
+               r2 <- regP
+               i <- intP
+               return $ op r1 r2 i
           -- Insns that take 1 Imm arg
-          regImmOpP = undefined
+          regImmOpP = 
+            do op <- constP "TRAP" TRAP
+               i  <- intP
+               return $ op i
+          -- Takes a reg and a label
+          regLblOpP = 
+            do op <- constP "LEA" LEA <|> constP "LC" LC
+               r1 <- regP
+               l  <- labelP
+               return $ op r1 l
+          -- Takes a label
+          lblOpP = 
+            do op <- constP "JSR" JSR <|> constP "JMP" JMP
+               l <- labelP 
+               return $ op l
+          
 
 -- | Parses an LC4 Comment. 
 -- TODO: can be any alphanumeric and/or more characters. 
@@ -73,22 +125,6 @@ commentP =
   do _delim <- many1 $ char ';'
      com <- many alpha
      return com
-
--- | Parses an LC4 hexadecimal representation.
-hexP :: Parser Int
-hexP = do _ <- char 'x'
-          i <- int
-          return i
-
--- | Parses an LC4 decimal representation. 
-decP :: Parser Int
-decP = do _ <- char '#'
-          i <- int
-          return i
-          
--- | Parses an LC4 integer of any representation.
-intP :: Parser Int
-intP = hexP <|> decP
 
 -- | Parses an LC4 directive
 directiveP :: Parser Directive
@@ -122,6 +158,8 @@ regP = char 'R' >>
          7 -> R7
          _ -> error "Wrong register identifier!"
 
+-- | Parses an LC4 label
+-- TODO: Fix rules about what chars are valid. Can't start with digit either.
 labelP :: Parser Label
 labelP = many1 (alpha <|> digit)
 
