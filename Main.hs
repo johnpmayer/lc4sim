0e1b7da926ea1d21eaaffb9b614fa7abd8522e57
John P Mayer, Jr <john.p.mayer.jr@gmail.com>
2011-12-14 00:31:52 -0500

    big moves on the REPLLEA doesn't parse correctly

--- Main.hs
+++ Main.hs
@@ -8,12 +8,15 @@ import System.Exit (exitSuccess, exitFailure)
 import System.IO
 import System (getArgs)
 import Data.Char
+import qualified Data.Map as M
+import Data.Maybe
 import Control.Monad.State
 
 import LC4Parser
 import LC4VM
 import ParserCombinators
 import VMLoader
+import Simulator
 
 -- | The welcome message. This is printed when the interpreter is started
 --   without any command-line arguments. 
@@ -32,23 +35,56 @@ helpText =
   "This is a list of commands available to you. Some commands expect \
   \an argument. \n \
   \\n \
-  \l | load <filename> -- load the asm file into the interpreter.\n \
   \q | quit -- Leave the interpreter.\n \
   \n | next -- Step to the next instruction.\n \
-  \p | print [arg] -- Print the system state. Optional arg will print \
-  \ that specific element.\n \
+  \r | reg  -- Show the register file.\n \
   \h | help -- Print this message.\n"
-
 
 -- | Processes a command from main and calls the appropriate 
 --   functions to deal with them.
-processCmd :: String -> IO ()
-processCmd cmd
+processCmd :: VMState -> String -> IO VMState
+processCmd s cmd
   | cmd `elem` ["quit", "q"] = exitSuccess
-  | cmd `elem` ["help", "h"] = putStrLn helpText
-  | cmd `elem` ["n", "next"] = putStrLn "This will step next"
-  | cmd `elem` ["l", "load"] = putStrLn "This will load the file" 
-  | otherwise = putStrLn "Command not recognized. Type help or h for help."
+  | cmd `elem` ["help", "h"] = putStrLn helpText >> return s
+  | cmd `elem` ["n", "next"] = nextInstruction s
+  | cmd `elem` ["r", "reg", "registers"] = showRegisters s >> return s
+  | cmd `elem` ["p", "prog"] = showProgram s >> return s
+  | cmd `elem` ["m", "mem", "memory"] = showMemory s >> return s
+  | cmd `elem` ["l", "lbl", "lables"] = showLabels s >> return s
+  | otherwise = 
+      putStrLn "Command not recognized. Type help or h for help." >> 
+        return s
+
+showLabels :: VMState -> IO ()
+showLabels s =
+  do
+    putStrLn "Labels"
+    putStrLn . show . LC4VM.lbls $ s
+
+showMemory :: VMState -> IO ()
+showMemory s =
+  do
+    putStrLn "Memory"
+    putStrLn . show . memory $ s
+
+showProgram :: VMState -> IO ()
+showProgram s =
+  do
+    putStrLn "Program Counter"
+    putStrLn . show . pc $ s
+    putStrLn (case M.lookup (pc s) (LC4VM.prog s) of
+      Nothing -> "No valid insn"
+      Just insn -> show insn)
+
+showRegisters :: VMState -> IO ()
+showRegisters s = 
+  do
+    putStrLn "Regfile"
+    let registers = regFile s
+    putStrLn $ show registers
+
+nextInstruction :: VMState -> IO VMState
+nextInstruction s = return $ execState nextStep s
 
 repl :: VMState -> IO ()
 repl s = 
@@ -56,9 +92,9 @@ repl s =
     putStr prompt
     hFlush stdout
     cmd <- getLine
-    processCmd (map toLower cmd)
     putStrLn $ "You typed " ++ cmd ++ ". "
-    repl s
+    s' <- processCmd s (map toLower cmd)
+    repl s'
     
 mainHelp :: String
 mainHelp = "LC4 Interpreter. Usage: lc4sim <filename.asm>"
@@ -67,7 +103,7 @@ parseLinesFromFile :: String -> IO ([Either ParseError Line])
 parseLinesFromFile filename = do
   handle <- openFile filename ReadMode
   contents <- hGetContents handle
-  putStrLn $ show $ lines contents
+  --putStrLn $ show $ lines contents
   return $ fmap (\lineS -> parse lineP lineS) (lines contents)
 
 checkParsedLines :: ([Either ParseError Line]) -> IO [Line]
@@ -81,6 +117,15 @@ checkParsedLines (Right l : ls) =
     ls <- checkParsedLines ls
     return $ l : ls
 
+run :: String -> IO ()
+run filename = 
+  do
+    prelines <- parseLinesFromFile filename
+    lines <- checkParsedLines prelines
+    putStrLn . unlines $ fmap show lines
+    putStrLn welcomeMsg
+    repl $ load lines
+
 -- | The main REPL loop.
 main :: IO ()
 main = do 
@@ -90,9 +135,8 @@ main = do
     putStrLn "Incorrect number of arguments, type --help for help."
     else 
     do 
-      let arg = head args
-      prelines <- parseLinesFromFile arg
-      lines <- checkParsedLines prelines
-      putStrLn $ show lines
-      repl $ load lines
+      let filename = head args
+      run filename
 
+test :: IO ()
+test = run "fib.asm"
